a3c74186302da90923c4ac908e7dcb3b597df46c
Thomas Heaton <thomas.m.heaton@gmail.com>
2021-11-12 15:11:47 +0000

    Initial Commit :bulb:

---
+++ src/main.cpp
@@ -0,0 +1,79 @@
+#include <Arduino.h>
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
+#define BUTTON_PIN 3
+#define LED_PIN 10
+
+#define MASTER 0
+
+RF24 radio(7, 8);  // CE, CSN
+
+const byte addresses[][6] = {"00001", "00002"};
+
+bool buttonStateA = 0;
+bool buttonStateB = 0;
+
+void A(void);
+void B(void);
+
+void setup() {
+    Serial.begin(9600);
+    Serial.println("starting...");
+
+    pinMode(BUTTON_PIN, INPUT_PULLUP);
+    pinMode(LED_PIN, OUTPUT);
+
+    radio.begin();
+
+    radio.openWritingPipe(addresses[MASTER ? 0 : 1]);
+    radio.openReadingPipe(1, addresses[MASTER ? 1 : 0]);
+
+    radio.setPALevel(RF24_PA_MIN);
+}
+
+void loop() {
+    MASTER ? A() : B();
+}
+
+void A() {
+    delay(5);
+
+    radio.stopListening();
+
+    buttonStateA = digitalRead(BUTTON_PIN);
+
+    radio.write(&buttonStateA, sizeof(buttonStateA));
+
+    delay(5);
+
+    radio.startListening();
+
+    while (!radio.available());
+
+    radio.read(&buttonStateB, sizeof(buttonStateB));
+
+    digitalWrite(LED_PIN, buttonStateA);
+}
+
+void B() {
+    delay(5);
+
+    radio.startListening();
+
+    if (radio.available()) {
+
+        radio.read(&buttonStateA, sizeof(buttonStateA));
+        
+        digitalWrite(LED_PIN, buttonStateA);
+
+        delay(5);
+
+        radio.stopListening();
+
+        buttonStateB = digitalRead(buttonStateB);
+
+        radio.write(&buttonStateB, sizeof(buttonStateB));
+    }
+}
