55d29ce596a3cb3f07f3a50110b408a92baff64d
Thomas Heaton <thomas.m.heaton@gmail.com>
2021-11-29 11:47:11 +0000

    i dont know what this is

--- src/transmitter.cpp
+++ src/transmitter.cpp
@@ -50,6 +50,7 @@ void setup()
     //   _radio.init(RADIO_ID, PIN_RADIO_CE, PIN_RADIO_CSN, NRFLite::BITRATE250KBPS, 0)
     
     if (!_radio.init(RADIO_ID, PIN_RADIO_CE, PIN_RADIO_CSN))
+    //if (!_radio.init(RADIO_ID, PIN_RADIO_CE, PIN_RADIO_CSN, NRFLite::BITRATE250KBPS, 0))
     {
         Serial.println("Cannot communicate with radio");
         while (1); // Wait here forever.
